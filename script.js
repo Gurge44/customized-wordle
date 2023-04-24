c790eca9f8288b2e6da1ba36f9dbdd82e7c8c523
Gurge44 <90392754+Gurge44@users.noreply.github.com>
2023-04-24 12:00:44 +0200

    Remove answer sending to console

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ let guessesRemaining = NUMBER_OF_GUESSES;
 let currentGuess = [];
 let nextLetter = 0;
 let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]
-console.log(rightGuessString)
 
 function initBoard() {
     let board = document.getElementById("game-board");
